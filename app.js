d144df2f331d147ca7472686f39bbc8e63032528
Yarden <yardenvdg@gmail.com>
2021-10-25 16:28:58 +0300

    nothing really

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const startBtn = document.querySelector(".start");
 const grid = document.querySelector(".grid");
 const squares = [];
 const snake = [2, 1, 0];
+const width = 10;
+let direction = 1;
 let scoreDisplay = document.querySelector(".score");
 
 function createGrid() {
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < width*width; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     grid.append(cell);
@@ -17,3 +19,16 @@ function createGrid() {
 createGrid();
 
 snake.forEach(index => squares[index].classList.add("snake"));
+
+function move() {
+
+  const tail = snake.pop();
+  squares[tail].classList.remove("snake");
+  snake.unshift(snake[0] + direction);
+  squares[snake[0]].classList.add("snake");
+
+}
+move();
+
+let movement = setInterval(move, 1000);
+
